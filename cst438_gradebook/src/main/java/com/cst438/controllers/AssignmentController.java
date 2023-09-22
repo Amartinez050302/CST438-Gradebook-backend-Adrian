1538e1079c06c7b57084a5a8b9183dc0611237ec
bayrichie707 <19826152+bayrichie707@users.noreply.github.com>
2023-09-21 18:10:24 -0700

    Comments added AssignmentController.java
    Added comments on assignmentController

--- cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
@@ -15,6 +15,10 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
+    
+// Class names abide by correct coding conventions: class name starts with an uppercase
+// Method and variable names abide by correct coding conventions: camel case conventions
+// Has all the required methods
 public class AssignmentController {
 
     @Autowired
@@ -23,8 +27,11 @@ public class AssignmentController {
     @Autowired
     CourseRepository courseRepository;
 
-    
 
+    // POST method's logic and functionality are correct: create a new assignmentDTO object and set the values.
+    // Called courseRepository and stores AssignmentDTO in courseRepo database
+    // Provides exception if adding a NULL value to the course
+    // Returns correct object 
     @PostMapping("/assignment")
     @ResponseStatus(HttpStatus.CREATED)
     public AssignmentDTO createAssignment(@RequestBody AssignmentDTO assignmentDTO) {
@@ -48,6 +55,9 @@ public class AssignmentController {
                 assignment.getCourse().getTitle(), assignment.getCourse().getCourse_id());
     }
 
+    // GET method's logic is correct and the code readability is good.
+    // Code comprehensibility is good: can understand method return value and arguments via @PathVariable is being passed from the URL
+    // Code comments are present and provide clarification on code logic
     @GetMapping("/assignment/{id}")
     public AssignmentDTO getAssignmentById(@PathVariable("id") Integer assignmentId) {
         // Retrieve the assignment by its ID
@@ -62,6 +72,10 @@ public class AssignmentController {
                 assignment.getCourse().getTitle(), assignment.getCourse().getCourse_id());
     }
 
+    // Provides PUT method that passes an ID and updates the assignment with relevant fields
+    // Exception is provided if the assignment object from assignmentRepository is of NULL value
+    // Returns AssignmentDTO object with updated values.
+    // Throws exception with HTTPStatus is assignment ID passed from URL cannot be found and/or is of NULL value
     @PutMapping("/assignment/{id}")
     public AssignmentDTO updateAssignment(@PathVariable("id") Integer assignmentId,
             @RequestBody AssignmentDTO assignmentDTO) {
@@ -91,6 +105,9 @@ public class AssignmentController {
                 assignment.getCourse().getTitle(), assignment.getCourse().getCourse_id());
     }
 
+    // Provides DELETE method that passes ID from URL and searches assignment repository for the given ID
+    // Throws exception with HTTPstatus if ID is not found in the assignment repository
+    // Deletes given assignment in assignment repository
     @DeleteMapping("/assignment/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteAssignment(@PathVariable("id") Integer assignmentId) {
