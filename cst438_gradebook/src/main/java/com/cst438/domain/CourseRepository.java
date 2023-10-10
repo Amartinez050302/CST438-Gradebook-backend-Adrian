8fcece0611a094e7559285cde7d9415363fe6395
Adrian <adrianmartinez1@csumb.edu>
2023-10-10 14:21:05 -0700

    Completed sections for assignment 4

--- cst438_gradebook/src/main/java/com/cst438/domain/CourseRepository.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/CourseRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CourseRepository extends CrudRepository <Course, Integer> {
 
+	Course findByCourse_id(Integer course_id);
+
 }
